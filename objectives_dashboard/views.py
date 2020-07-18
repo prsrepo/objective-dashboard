f3d2bd95ec1ad67201df8c7be302f01526ab0b1f
puru <kspreddy94@gmail.com>
2020-07-18 13:35:32 +0530

    inprogress

--- objectives_dashboard/views.py
+++ objectives_dashboard/views.py
@@ -1,3 +1,51 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+# from .forms import MyForm
+
+class ObjectivesSummary(View):
+    # form_class = MyForm
+    def get(self, request):
+        return HttpResponse({
+            "data": {
+                "objectives": 80,
+                "completed": 60,
+                "start_time": "15-06-2020"
+            }
+        })
+
+class Departments(View):
+    def get(self, request):
+        return HttpResponse({
+            "data": [
+                {
+                    "name": "sales",
+                    "objectives": 10,
+                    "no_of_employees": 2
+                },
+                {
+                    "name": "sales",
+                    "objectives": 10,
+                    "no_of_employees": 2
+                }
+            ]
+        })
+
+
+class Teams(View):
+    def post(self, request):
+        return HttpResponse({
+            "data": [
+                {
+                    "lead": "alex",
+                    "objectives": 10,
+                    "no_of_employees": 2
+                },
+                {
+                    "lead": "sales",
+                    "objectives": 10,
+                    "no_of_employees": 2
+                }
+            ]
+        })
