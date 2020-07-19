00c5cc9b93d9341d28cfadbff9d1e3e3efe09c12
Purushotham Reddy <puroshotham.k@noodle.ai>
2020-07-19 22:11:54 +0530

    #removed admin page

--- track_objectives/urls.py
+++ track_objectives/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # uncomment below line to enable admin
+    # path('admin/', admin.site.urls),
     path('', include('objectives_dashboard.urls'))
 ]
 
